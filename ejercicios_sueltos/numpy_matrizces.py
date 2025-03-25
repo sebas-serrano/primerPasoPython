c63674909417900b34035ba836ee714275d36f03
sebas-serrano <serranoavila@gmail.com>
2025-03-25 12:15:08 -0300

    nuevos ejercicios de nunpy

---
+++ ejercicios_sueltos/numpy_matrizces.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+n = int(input("Ingrese la cantidad de filas de la matriz: "))
+m = int(input("Ingrese la cantidad de columnas de la matriz: "))
+
+
+A = np.empty((n,m))
+
+
+for i in range(n):
+    for j in range(m):
+        A[i,j] = float(input("Ingrese el elemento ({},{}): ".format(i,j)))
+
+print("\n=== Matriz ===")
+print(A)
